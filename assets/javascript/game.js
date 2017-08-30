60a4672ccefd0816fe37358209b2cd228baf8355
TJ Cooper <tjcoop321@gmail.com>
2017-08-30 14:16:04 -0700

    fix js file to make game finally work as intended; can now work on prettying it up

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,8 +2,10 @@
 window.onload = function() {
     // create object to store different properties and functions I need
     var game = {
+        state: true,
         lettGuessed: [],
-        counter: 0,
+        correctGuesses: 0,
+        guessesLeft: 10,
         wordBank: ["PickleRick", "MrPoopyButtHole", "Morty", "Summer", "Beth", "BirdPerson"],
         currentWord: "",
         press: "",
@@ -15,26 +17,55 @@ window.onload = function() {
             return this.currentWord;
         },
 
+        tallyGuesses: function () {
+            if(this.state && this.guessesLeft > 0){
+                // count correct guesses
+                if(this.currentWord.indexOf(this.press) !== -1) {
+                    this.correctGuesses++;
+                    document.querySelector("#guessesLeft").innerHTML = this.guessesLeft;
+                } else {
+                    this.guessesLeft--;
+                    document.querySelector("#guessesLeft").innerHTML = this.guessesLeft;
+                }
+            } else if (this.state && this.guessesLeft === 0){
+                alert("you lose, try again");
+                this.state = false;
+            }
+        },
+
         wordRender: function() {
             var wordSpace = "";
-            for (i = 0; i < game.currentWord.length; i++) {
-                if (game.lettGuessed.indexOf(game.currentWord[i]) !== -1) {
-                    wordSpace += game.currentWord[i];
+            for (i = 0; i < this.currentWord.length; i++) {
+                if (this.lettGuessed.indexOf(this.currentWord[i]) !== -1) {
+                    wordSpace += this.currentWord[i];
                 } else {
                     wordSpace += " _";
                 }
             }
+            if(wordSpace!==this.currentWord){
+                this.state = true;
+                this.tallyGuesses();
+            } else if (wordSpace === this.currentWord) {
+                this.state = false;
+                alert("you win!");
+            }
             console.log(wordSpace);
             document.querySelector("#currWord").innerHTML = wordSpace;
         }
+
     };
 
-    /*Calling Functions and event listeners below this line*/
+/*Calling Functions and event listeners below this line*/
 
     // call this function to generate random word
+
     game.wordPicked();
+
     // call this function to generate empty spaces for the word selected
+
     game.wordRender();
+
+    // wait for user to presss a key and push that letter to an array of guesses
 
     document.onkeypress = function(event) {
         game.press = event.key;
@@ -43,30 +74,13 @@ window.onload = function() {
             alert("you've already guessed that")
         } else {
             game.lettGuessed.push(chosenLetter);
-            game.counter++;
         };
         // display letters guessed within HTML 
         document.querySelector("#lettersGuessed").innerHTML = game.lettGuessed;
         console.log(game.press);
-        // call this function again after onkeypress to see if keys match the word
+        // see if keys match the word
         game.wordRender();
     }
 
 
 }
-
-
-// var wordSpace = "";
-//     for (i = 0; i < game.currentWord.length; i++) {
-//         if (game.press.indexOf(game.currentWord[i]) !== -1) {
-//             wordSpace = game.currentWord[i];
-//         } else {
-//             wordSpace = wordSpace + " _";
-//         }
-//     }
-//     document.querySelector("#currWord").innerHTML = wordSpace;
-
-
-// record key pressed and store in property press of game object
-// document.onkeypress = function(event) {
-// 	game.press = event.key;
